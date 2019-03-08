bbe626a47faae3fc7f6b170f1afb5821624da7e4
Vinod Kisanagaram <kvr2277@gmail.com>
2019-03-08 06:52:43 -0500

    Call Age Claculator

--- api/dayOfBirth.js
+++ api/dayOfBirth.js
@@ -1,20 +1,46 @@
 'use strict';
 
 let moment = require('moment');
+let fetch = require('node-fetch');
 
-module.exports.submit = (event, context, callback) => {
+module.exports.submit = async (event, context, callback) => {
 
   let ageCalcRequest = event.queryStringParameters;
   let dob = ageCalcRequest.dob;
-  
+  let resp = await fetchAge(dob).then(resp => {return resp});
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      day: moment(dob, 'YYYYMMDD').format('dddd')
+      day: moment(dob, 'YYYYMMDD').format('dddd'),
+      age: resp.age
     }),
   };
 
   callback(null, response);
-
 };
+
+async function fetchAge(dob) {
+  return new Promise((resolve, reject) => {
+    
+      let url = 'https://xxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/age?dob=' + dob;
+      fetch(url, {
+              method: 'GET',
+              headers: {
+            Accept: 'application/json'
+        }})
+          .then(res => {
+              if (res.ok) {
+                  res.text().then(function(text) {
+                      resolve(text ? JSON.parse(text) : {});
+                  });
+              } else {
+                  console.log('looks like the response was not perfect, got status' + res.status);
+                  reject('request failed ', res.statusText);
+              }
+          }, function(e) {
+              console.log('fetch failed!', e);
+              reject('fetch failed ', e);
+          });
+  });
+}
